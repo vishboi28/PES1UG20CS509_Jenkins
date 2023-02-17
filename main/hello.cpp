69c41eeeb4b1b4a87307b7273cc4fc4aefefdd49
Vishnu Athreya <vishnu.athreya77@gmail.com>
2023-02-17 23:04:14 +0530

    committed successfully

---
+++ main/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
