2d6150d2c43edb0f97748717d7fce654c821b3af
vishboi28 <76418267+vishboi28@users.noreply.github.com>
2023-02-19 21:16:55 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "I am also Vishnu Athreya\n";
+  cout << "I am also Vishnu\n";
   return 0;
 }
